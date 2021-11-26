60c2e51c302d034c9458cfa18583197f85f54f71
David <raymondinf23@gmail.com>
2021-11-26 00:09:55 -0400

    Add loading component

--- src/pages/AuthorQuotes/index.jsx
+++ src/pages/AuthorQuotes/index.jsx
@@ -1,26 +1,44 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import { getQuotesOfAuthor } from '../../api/QuoteApi'
-import QuoteCard from '../../components/QuoteCard';
+import QuoteCard from '../../components/QuoteCard'
+import Loading from '../../components/Loading'
+import ArrowRight from '../../assets/icons/arrow-right.svg'
 
 import styles from './styles.module.css'
 
 const AuthorQuotes = ({ quote }) => {
     const [ authorQuotes, setAuthorQuotes ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
 
-    useEffect( async()=> {
+    useEffect( async() => {
+        setLoading(true)
         const data = await getQuotesOfAuthor(quote.quoteAuthor)
         setAuthorQuotes(data)
+        setLoading(false)
     }, [quote])
 
     return (
         <main className={ styles["author-quotes-container"] } >
-            <h1>{ quote.quoteAuthor }</h1>
-            <section className={ styles["author-quotes"] } >
-                {
-                    authorQuotes.map(quote => <QuoteCard key={ quote._id } quote={ quote } />)
-                }
-            </section>
+            <Link to="/" style={{ width: '50px' }} >
+                <img src={ ArrowRight } alt="" className={ styles["arrow-icon"] } />
+            </Link>
+            {
+                loading ? 
+                    <Loading />
+                :
+                    (
+                        <section className={ styles["author-quotes"] } >
+                            <h1 className={ styles["author-name"] } >
+                                { quote.quoteAuthor }
+                            </h1>
+                            {
+                                authorQuotes.map(quote => <QuoteCard key={ quote._id } quote={ quote } />)
+                            }
+                        </section>
+                    )
+            }
         </main>
     )
 };
